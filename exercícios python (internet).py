3594b07b3ce676930640bc4de9863c35dc8181ab
matheusarauj76 <matheussousa_araujo@hotmail.com>
2024-11-07 22:02:51 -0300

    Update exercícios python (internet).py

--- exercícios python (internet).py
+++ exercícios python (internet).py
@@ -84,11 +84,24 @@
 # elif sexo == "mulher":
 #     print(f"Você é mulher, seu peso ideal é de {peso_ideal_m}")
 
-# João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar. Imprima os dados do programa com as mensagens adequadas.
+# João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior 
+# que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa 
+# que leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João 
+# deverá pagar. Imprima os dados do programa com as mensagens adequadas.
 
+# peso_peixes = float(input("Quantos kg de peixes você pescou?"))
+# peso_regulamento = 50
+# peso_excedido = peso_peixes - peso_regulamento
+# multa = round(peso_excedido * 4, 2)
+# multa_formatada = f"{multa:.2f}"
 
-
-# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+# if peso_peixes > 50:
+#     print(f"Você informou que pescou um total de {peso_peixes}kg. Como o regulamento prevê o limite de até {peso_regulamento}kg, você ultrapassou {peso_excedido}kg, gerando uma multa de R$ {multa_formatada}.")
+# else:
+#     print(f"Você informou que pescou um total de {peso_peixes}kg, não excedeu o regulamento de {peso_regulamento}kg, portanto não pagará nenhuma multa.")
+                    
+# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês,
+# sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
 # salário bruto.
 # quanto pagou ao INSS.
 # quanto pagou ao sindicato.
@@ -100,7 +113,35 @@
 # - Sindicato ( 5%) : R$
 # = Salário Liquido : R$
 # Obs.: Salário Bruto - Descontos = Salário Líquido.
-# Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
+
+# salario_hora = float(input("Quanto você ganha por hora?"))
+# horas_trabalhadas = float(input("Quantas horas você trabalhou no mês? (Um clt comum que trabalha 40 horas semanais trabalha em média 200 horas mensais)."))
+# salario_bruto_mes = salario_hora * horas_trabalhadas
+# salario_bruto = f"{salario_bruto_mes:.2f}"
+# inss_mes = (salario_bruto_mes / 100 * 8)
+# inss = f"{inss_mes:.2f}"
+# sindicato_mes = salario_bruto_mes / 100 * 5
+# sindicato = f"{sindicato_mes:.2f}"
+# ir_mes = salario_bruto_mes / 100 * 11
+# ir = f"{ir_mes:.2f}"
+# descontos_mes = inss_mes + ir_mes + sindicato_mes
+# descontos = f"{descontos_mes:.2f}"
+# salario_liquido_mes = salario_bruto_mes - descontos_mes
+# salario_liquido = f"{salario_liquido_mes:.2f}"
+# print(f"Seu salário bruto no mês é de R$ {salario_bruto}. Porém, você recebe vários descontos, como INSS: R$ {inss}, IR: R$ {ir}, desconto sindical: R$ {sindicato}. Com todos esses descontos (que somam R$ {descontos}, seu salário líquido é de {salario_liquido})")
+
+# Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada
+# 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
+
+area = float(input("Qual o tamanho (em metros quadrados) da área a ser pintada?"))
+l_tinta = 18
+v_tinta = 80
+l_quadrado = 1 / 3
+l_tinta_area_pintada = l_tinta * area
+v_tinta_area_pintada = v_tinta * area
+print(f"Com uma área de {area} metros quadrados, você precisa de {l_tinta_area_pintada} litros de tinta, e gastará o total de R${v_tinta_area_pintada}.")
+
+
 # Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00 ou em galões de 3,6 litros, que custam R$ 25,00.
 # Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
 # comprar apenas latas de 18 litros;
